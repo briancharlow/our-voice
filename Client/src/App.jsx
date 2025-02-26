7695a27b1322ebeff274722982a2f41ced16584a
Charlow <briankyalo416@gmail.com>
2025-02-26 12:54:36 +0300

    Initial Commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import CitizenConnect from './components/citizen'
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+    
+      <CitizenConnect />
+    </div>
+    
+  )
+}
+
+export default App
